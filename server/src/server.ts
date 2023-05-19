5d8be6614d65c01066719c27a6c4821f8a017b01
Felipe Domingues de Miranda <felipedm215@hotmail.com>
2023-05-19 01:06:29 -0300

    Aula 4

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+// Define que a pasta de uploads é de acesso público
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // Todas URLs de front poderão acessar o back
@@ -15,8 +26,9 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime', // Usado para diferenciar os JWTs gerados por este back-end dos demais
 })
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
